e00c0ebc0d2b3c80ead9457d030566cbd9dceb3b
Lourenco a22406954 <a22406954@alunos.ulht.pt>
2024-09-25 11:38:52 +0100

    aula3

---
+++ Aula3/script1.bash
@@ -0,0 +1,22 @@
+#1/usr/bin/bash
+
+#script1: 1f
+echo "Nome="
+read nome
+echo "Nome=$nome"
+if [["$nome" == "$1"]]; then
+echo "OK: $nome = $1"
+else 
+echo "NOK: $nome != $1"
+fi
+
+echo "Nome="
+read nome 
+echo "Idade="
+read idade
+echo "Nome=$nome, Idade=$idade"
+if [[ "$nome" == "$1" ]] && [[ "$idade" -le $2 ]]; then 
+echo "OK: $nome == $1 && $idade < $2"
+else
+echo "NOK: $nome != $1 || idade >= $2"
+fi
