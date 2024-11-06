99cd4f84a96ca64065c76b7ee9bbe9ca717d5abb
Lourenco a22406954 <a22406954@alunos.ulht.pt>
2024-11-06 11:12:14 +0000

    agente meio feito

--- Trabalho2/suport_agent.c
+++ Trabalho2/suport_agent.c
@@ -1,44 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-int main(int argc, char *argv[]) {
-    const char *pipe;
-    if (argc == 2) {
-        pipe = argv[1];
-    } else {
-        pipe = "/tmp/support_pipe";
-    }
+#define FIFO_IN "/tmp/suporte"
+#define BSIZE 256
 
-    FILE *pipeFile = fopen(pipe, "r");
-    if (pipeFile == NULL) {
+int main() {
+    char msg_in[BSIZE];
+    char msg_out[BSIZE];
+
+    // Abre o named pipe para leitura
+    int fd = open(FIFO_IN, O_RDONLY);
+    if (fd == -1) {
         perror("Erro ao abrir o pipe");
         return 1;
     }
 
-    char request[256];
-    srand(time(NULL)); // Seed para gerar números aleatórios
+    while (1) {
+        // Lê a mensagem do pipe
+        ssize_t bytes_read = read(fd, msg_in, BSIZE);
+        if (bytes_read > 0) {
+            msg_in[bytes_read] = '\0';  // Garante que a string termina em '\0'
+            printf("Mensagem recebida: %s\n", msg_in);
 
-    while (fgets(request, sizeof(request), pipeFile) != NULL) {
-        // Remover o newline do final da string, se existir
-        request[strcspn(request, "\n")] = 0;
+            // Extrai o nome do pipe de resposta do conteúdo da mensagem recebida
+            // (Considerando que o nome do pipe está no final da mensagem recebida)
+            char nome_resp[BSIZE];
+            sscanf(msg_in, "%*s %*s %s", nome_resp); // Ignora os primeiros dois parâmetros e lê o nome do pipe de resposta
 
-        if (strcmp(request, "quit") == 0) {
-            printf("Support Agent: quit\n");
+            // Abre o named pipe de resposta para o student
+            int fd_out = open(nome_resp, O_WRONLY);
+            if (fd_out == -1) {
+                perror("Erro ao abrir o pipe de resposta");
+                continue;
+            }
+
+            // Envia a resposta
+            snprintf(msg_out, BSIZE, "Resposta do Support Agent");
+            write(fd_out, msg_out, strlen(msg_out) + 1);
+
+            // Fecha o pipe de resposta
+            close(fd_out);
+        } else if (bytes_read == 0) {
+            break;  // o pipe foi fechado
+        } else {
+            perror("Erro ao ler do pipe");
             break;
         }
-
-        printf("Support Agent processing: %s\n", request);
-
-        // Simula o tempo de processamento (1 a 5 segundos)
-        int delay = 1 + rand() % 5;
-        sleep(delay);
     }
 
-    fclose(pipeFile);
-    printf("Support Agent: read request terminated\n");
+    close(fd);
+    printf("Support Agent: Encerrado\n");
 
     return 0;
 }
